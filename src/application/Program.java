7aeae037f39f2d563f0c04aceb45b89a0c6d2164
Jarbas Santos <jarbas3@outlook.com>
2023-07-18 09:03:24 -0300

    Solução do professor Nélio Alves

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,22 +4,22 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entities.Student;
-
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
 		
-		Set<Student> set = new HashSet<>();
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
 		
 		System.out.print("How many students for course A? ");
 		int n = sc.nextInt();
 		
 		for (int i = 0; i < n; i++) {
 			int code = sc.nextInt();
-			set.add(new Student(code));
+			a.add(code);
 		}
 		
 		System.out.print("How many students for course B? ");
@@ -27,7 +27,7 @@ public class Program {
 		
 		for (int i = 0; i < n; i++) {
 			int code = sc.nextInt();
-			set.add(new Student(code));
+			b.add(code);
 		}
 		
 		System.out.print("How many students for course C? ");
@@ -35,10 +35,13 @@ public class Program {
 		
 		for (int i = 0; i < n; i++) {
 			int code = sc.nextInt();
-			set.add(new Student(code));
+			c.add(code);
 		}
 		
-		System.out.println("Total students: " + set.size());
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+	    System.out.println("Total students: " + total.size());
 		
 		sc.close();
 	}
