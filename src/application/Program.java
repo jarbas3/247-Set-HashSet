8da495c2887943b327db0978231c6ea2ad8a604b
Jarbas Santos <jarbas3@outlook.com>
2023-07-18 08:53:59 -0300

    Set e HashSet usando Classe personalizada

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,20 +4,22 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+import entities.Student;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
 		
-		Set<Integer> set = new HashSet<>();
+		Set<Student> set = new HashSet<>();
 		
 		System.out.print("How many students for course A? ");
 		int n = sc.nextInt();
 		
 		for (int i = 0; i < n; i++) {
 			int code = sc.nextInt();
-			set.add(code);
+			set.add(new Student(code));
 		}
 		
 		System.out.print("How many students for course B? ");
@@ -25,7 +27,7 @@ public class Program {
 		
 		for (int i = 0; i < n; i++) {
 			int code = sc.nextInt();
-			set.add(code);
+			set.add(new Student(code));
 		}
 		
 		System.out.print("How many students for course C? ");
@@ -33,7 +35,7 @@ public class Program {
 		
 		for (int i = 0; i < n; i++) {
 			int code = sc.nextInt();
-			set.add(code);
+			set.add(new Student(code));
 		}
 		
 		System.out.println("Total students: " + set.size());
